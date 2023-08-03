be4932176a4d3f547eebbf24c82f70d0d5196230
João Henrique <126480950+Joaohnt27@users.noreply.github.com>
2023-08-03 11:05:25 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,21 +3,20 @@ Faça um programa que receba uma frase, calcule e mostre a quantidade de vogais
 da frase digitada. O programa deverá contar vogais maiúsculas e minúsculas.
 */
 #include <stdio.h>
-#include <string.h>
 
 int main(void) {
-  char frase[15], letrasvogais[] = "AaEeIiOoUu";
-  int vogais = 0, j;
+  char frase[15];
+  int vogais = 0;
 
   printf("Digite uma frase qualquer: ");
   scanf("%[^\n]", frase);
 
   for (int i = 0; frase[i] != '\0'; i++) {
-      j = 0;
-      while (j < strlen(letrasvogais) && frase[i] != letrasvogais[j])
-          j++;
-      if (j < strlen(letrasvogais))
-        vogais++;
+    if (frase[i] == 'A' || frase[i] == 'a' || frase[i] == 'E' ||
+        frase[i] == 'e' || frase[i] == 'I' || frase[i] == 'i' ||
+        frase[i] == 'O' || frase[i] == 'o' || frase[i] == 'U' ||
+        frase[i] == 'u')
+      vogais++;
   }
 
   printf("A frase \"%s\" possui %d vogais.\n", frase, vogais);
